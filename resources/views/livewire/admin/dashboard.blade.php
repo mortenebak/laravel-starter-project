57baa4583db345ec31fb99b4aa27177b169d4373
Morten Bak <hej@netbums.dk>
2023-01-04 17:10:39 +0100

    edit user modal

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="flex justify-between items-center mb-5 h-10">
-        <h1 class="text-3xl font-bold">
+        <h1 class="text-3xl font-bold dark:text-gray-200">
             Dashboard
         </h1>
         <div>
--- resources/views/livewire/admin/users/edit-user.blade.php
+++ resources/views/livewire/admin/users/edit-user.blade.php
@@ -1,23 +1,30 @@
-<div class="p-4 w-full">
+<div class="p-4 w-full dark:bg-gray-900 flex flex-col space-y-4">
 
-    <h2>Edit user</h2>
 
-    <form>
-        <input type="hidden" wire:model="category_id">
-        <div class="form-group mb-3">
-            <label for="name">Name:</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Enter Name" wire:model="name">
-            @error('name') <span class="text-danger">{{ $message }}</span>@enderror
-        </div>
-        <div class="form-group mb-3">
-            <label for="email">email:</label>
-            <textarea class="form-control @error('email') is-invalid @enderror" id="email" wire:model="email" placeholder="Enter email"></textarea>
-            @error('email') <span class="text-danger">{{ $message }}</span>@enderror
-        </div>
-        <div class="d-grid gap-2">
-            <button wire:click.prevent="update()" class="btn btn-success btn-block">Save</button>
-            <button wire:click.prevent="cancel()" class="btn btn-danger">Cancel</button>
-        </div>
-    </form>
+    <h2 class="text-2xl font-bold dark:text-white">Edit user</h2>
+
+
+    <div class="form-group">
+        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+        <input id="name" wire:model="name" type="text"
+               class="@error('name') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+               placeholder="Enter name" required>
+        @error('name') <span class="text-danger">{{ $message }}</span>@enderror
+    </div>
+
+    <div class="form-group">
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail</label>
+        <input id="email" wire:model="email" type="text"
+               class="@error('email') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+               placeholder="Enter email" required>
+        @error('email') <span class="text-danger">{{ $message }}</span>@enderror
+    </div>
+
+    <div>
+        <button wire:click.prevent="update()"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Save user
+        </button>
+    </div>
 
 </div>
