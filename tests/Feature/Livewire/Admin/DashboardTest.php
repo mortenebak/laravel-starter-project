ad038704dd674ae340088560efe71788ef7dd057
Morten Bak <hej@netbums.dk>
2023-01-27 21:49:46 +0100

    add test for Dashboard access

---
+++ tests/Feature/Livewire/Admin/DashboardTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+test('super admin can access dashboard', function () {
+
+    $this->actingAs($this->user)
+        ->get(route('admin.dashboard'))
+        ->assertOk();
+
+});
+
+test('a user with a role that only has access dashboard can access the admin dashboard', function () {
+
+    $user = User::factory()->create();
+    $role = Role::create(['name' => 'test role']);
+    $user->assignRole($role);
+    $role->givePermissionTo('access dashboard');
+
+    $this->actingAs($user)
+        ->get(route('admin.dashboard'))
+        ->assertOk();
+
+});
+
+test('a user without a role that has access dashboard cannot access the dashboard', function () {
+
+    $user = User::factory()->create();
+
+    $this->actingAs($user)
+        ->get(route('admin.dashboard'))
+        ->assertForbidden();
+
+});
+
