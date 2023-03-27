edb6869007366cb00a0e64a6159008c0d1d5a647
Morten Bak <meb@indexed.dk>
2023-03-27 11:49:27 +0200

    add more dashboard tests

--- tests/Feature/Livewire/Admin/DashboardTest.php
+++ tests/Feature/Livewire/Admin/DashboardTest.php
@@ -5,8 +5,8 @@ use Spatie\Permission\Models\Role;
 
 test('super admin can access dashboard', function () {
     $this->actingAs($this->user)
-        ->get(route('admin.dashboard'))
-        ->assertOk();
+         ->get(route('admin.dashboard'))
+         ->assertOk();
 });
 
 test('a user with a role that only has access dashboard can access the admin dashboard', function () {
@@ -19,17 +19,62 @@ test('a user with a role that only has access dashboard can access the admin das
     $role->givePermissionTo('view permissions');
 
     $this->actingAs($user)
-        ->get(route('admin.dashboard'))
-        ->assertOk()
-    ->assertSee('Users')
-    ->assertSee('Permissions')
-    ->assertSee('Roles');
+         ->get(route('admin.dashboard'))
+         ->assertOk()
+         ->assertSee('Users')
+         ->assertSee('Permissions')
+         ->assertSee('Roles');
+});
+
+test('a user with a role that can view users, cannot view roles and permissions', function () {
+    $user = User::factory()->create();
+    $role = Role::create(['name' => 'test role']);
+    $user->assignRole($role);
+    $role->givePermissionTo('access dashboard');
+    $role->givePermissionTo('view users');
+
+    $this->actingAs($user)
+         ->get(route('admin.dashboard'))
+         ->assertOk()
+         ->assertSee('Users')
+         ->assertDontSee('Permissions')
+         ->assertDontSee('Roles');
+});
+
+test('a user with a role that can view roles, cannot view permissions', function () {
+    $user = User::factory()->create();
+    $role = Role::create(['name' => 'test role']);
+    $user->assignRole($role);
+    $role->givePermissionTo('access dashboard');
+    $role->givePermissionTo('view users');
+    $role->givePermissionTo('view roles');
+
+    $this->actingAs($user)
+         ->get(route('admin.dashboard'))
+         ->assertOk()
+         ->assertSee('Roles')
+         ->assertDontSee('Permissions');
+});
+
+test('a user with a role that can view permissions, cannot view roles', function () {
+    $user = User::factory()->create();
+    $role = Role::create(['name' => 'test role']);
+    $user->assignRole($role);
+    $role->givePermissionTo('access dashboard');
+    $role->givePermissionTo('view users');
+    $role->givePermissionTo('view permissions');
+
+    $this->actingAs($user)
+         ->get(route('admin.dashboard'))
+         ->assertOk()
+         ->assertSee('Permissions')
+         ->assertDontSee('Roles');
 });
 
 test('a user without a role that has access dashboard cannot access the dashboard', function () {
     $user = User::factory()->create();
 
     $this->actingAs($user)
-        ->get(route('admin.dashboard'))
-        ->assertForbidden();
+         ->get(route('admin.dashboard'))
+         ->assertForbidden();
 });
