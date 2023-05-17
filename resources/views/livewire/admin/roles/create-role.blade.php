53c2094f038e37188e13dadc5652acf4c4fecda3
Morten Bak <meb@indexed.dk>
2023-05-17 16:21:40 +0200

    update spelling

--- resources/views/livewire/admin/roles/create-role.blade.php
+++ resources/views/livewire/admin/roles/create-role.blade.php
@@ -24,6 +24,6 @@
     </div>
 
     <div>
-        <button wire:click.prevent="create()" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save permission</button>
+        <button wire:click.prevent="create()" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Role</button>
     </div>
 </div>
