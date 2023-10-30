2a2679842118a74710e970956219cc44e1c0791d
Morten Bak <meb@indexed.dk>
2023-10-30 14:50:55 +0100

    change up seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,24 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             PermissionSeeder::class,
-        ]);
-
-        $user = User::create([
-            'name' => 'Admin',
-            'email' => 'meb@indexed.dk',
-            'password' => bcrypt('password'),
-        ]);
-        $user->assignRole('Super Admin');
-
-        Plan::query()->create([
-            'title' => 'Pro - 39 DKK / måned',
-            'slug' => 'pro-monthly',
-            'stripe_id' => 'price_1M9oZfKePSHD5WYkw7y8wq1R',
-        ]);
-        Plan::query()->create([
-            'title' => 'Pro - 399 DKK / år',
-            'slug' => 'pro-yearly',
-            'stripe_id' => 'price_1M9oZfKePSHD5WYk4ZM245Hv',
+            PlanSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/PlanSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Plan;
+use Illuminate\Database\Seeder;
+
+class PlanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (!Plan::query()->where('slug', '=', 'pro-monthly')->exists()) {
+            Plan::query()->create([
+                'title' => 'Pro - 39 DKK / måned',
+                'slug' => 'pro-monthly',
+                'stripe_id' => 'price_1M9oZfKePSHD5WYkw7y8wq1R',
+            ]);
+        }
+        if (!Plan::query()->where('slug', '=', 'pro-yearly')->exists()) {
+            Plan::query()->create([
+                'title' => 'Pro - 399 DKK / år',
+                'slug' => 'pro-yearly',
+                'stripe_id' => 'price_1M9oZfKePSHD5WYk4ZM245Hv',
+            ]);
+        }
+    }
+}
