a7b3869c4a3c24815e92dd64527019e3ea415975
Morten Bak <hej@netbums.dk>
2025-09-28 10:14:17 +0200

    update admin dashboard with placeholders

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,14 +1,36 @@
 <div>
-    <div class="flex justify-between items-center mb-5 h-10">
-        <h1 class="text-3xl font-bold dark:text-stone-200">
-            {{ __('Dashboard') }}
-        </h1>
+    <div class="flex justify-between items-center mb-5">
+        <div class="flex flex-col gap-2">
+            <h1 class="text-3xl font-bold dark:text-neutral-200">
+                {{ __('Dashboard') }}
+            </h1>
+            <x-p>
+                {{ __('Welcome back, :name', ['name' => auth()->user()->name]) }}
+            </x-p>
+        </div>
         <div>
 
         </div>
     </div>
-    <div class="dark:text-slate-200">
-        {{ __('To attain knowledge, add things every day; To attain wisdom, subtract things every day.') }}
+    <x-p class="dark:text-neutral-200">
+        {{ __('Here is a quick overview of your application\'s statistics and recent activities.') }}
+    </x-p>
+
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl min-h-[40rem]">
+        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20"/>
+            </div>
+            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20"/>
+            </div>
+            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20"/>
+            </div>
+        </div>
+        <div class="relative h-full flex-1 rounded-xl border border-neutral-200 dark:border-neutral-700">
+            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20"/>
+        </div>
     </div>
 </div>
 
